b0e831bba06b888522adc5d487a3d08460106665
IvetteMo <tp984@correo.tij.ibero.mx>
2023-09-09 19:26:35 -0700

    Agregar archivos de proyecto.

---
+++ validacionesTT/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace validacionesTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtDescr = new System.Windows.Forms.TextBox();
+            this.lblDescr = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.lklSalir = new System.Windows.Forms.LinkLabel();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.ttMensaje = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.gbDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.BackColor = System.Drawing.Color.LavenderBlush;
+            this.gbDatos.Controls.Add(this.button1);
+            this.gbDatos.Controls.Add(this.btnEnviar);
+            this.gbDatos.Controls.Add(this.txtDescr);
+            this.gbDatos.Controls.Add(this.lblDescr);
+            this.gbDatos.Controls.Add(this.txtSalario);
+            this.gbDatos.Controls.Add(this.lblSalario);
+            this.gbDatos.Controls.Add(this.txtEdad);
+            this.gbDatos.Controls.Add(this.lblEdad);
+            this.gbDatos.Controls.Add(this.lklSalir);
+            this.gbDatos.Controls.Add(this.btnNuevo);
+            this.gbDatos.Controls.Add(this.txtNombre);
+            this.gbDatos.Controls.Add(this.lblNombre);
+            this.gbDatos.Location = new System.Drawing.Point(12, 12);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(661, 351);
+            this.gbDatos.TabIndex = 1;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Ingrese Datos";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(520, 231);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(111, 48);
+            this.btnEnviar.TabIndex = 10;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtDescr
+            // 
+            this.txtDescr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescr.Location = new System.Drawing.Point(142, 203);
+            this.txtDescr.Multiline = true;
+            this.txtDescr.Name = "txtDescr";
+            this.txtDescr.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescr.Size = new System.Drawing.Size(242, 124);
+            this.txtDescr.TabIndex = 9;
+            // 
+            // lblDescr
+            // 
+            this.lblDescr.AutoSize = true;
+            this.lblDescr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescr.Location = new System.Drawing.Point(31, 206);
+            this.lblDescr.Name = "lblDescr";
+            this.lblDescr.Size = new System.Drawing.Size(109, 22);
+            this.lblDescr.TabIndex = 8;
+            this.lblDescr.Text = "Descripción:";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(142, 153);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(122, 28);
+            this.txtSalario.TabIndex = 7;
+            this.txtSalario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSalario_KeyPress);
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(31, 156);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(71, 22);
+            this.lblSalario.TabIndex = 6;
+            this.lblSalario.Text = "Salario:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEdad.Location = new System.Drawing.Point(142, 107);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(122, 28);
+            this.txtEdad.TabIndex = 5;
+            this.txtEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEdad_KeyPress);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(31, 110);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(57, 22);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // lklSalir
+            // 
+            this.lklSalir.AutoSize = true;
+            this.lklSalir.Location = new System.Drawing.Point(597, 18);
+            this.lklSalir.Name = "lklSalir";
+            this.lklSalir.Size = new System.Drawing.Size(34, 16);
+            this.lklSalir.TabIndex = 3;
+            this.lklSalir.TabStop = true;
+            this.lklSalir.Text = "Salir";
+            this.lklSalir.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklSalir_LinkClicked);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Location = new System.Drawing.Point(520, 177);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(111, 48);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(142, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(242, 28);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(31, 67);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(78, 22);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(520, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 48);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 374);
+            this.Controls.Add(this.gbDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbDatos;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtDescr;
+        private System.Windows.Forms.Label lblDescr;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.LinkLabel lklSalir;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.ToolTip ttMensaje;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ validacionesTT/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace validacionesTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ttMensaje.SetToolTip(this.txtNombre, "Ingrese un nombre");
+            this.ttMensaje.SetToolTip(this.txtEdad, "Ingrese edad");
+            this.ttMensaje.SetToolTip(this.txtSalario, "Ingrese salario");
+            this.ttMensaje.SetToolTip(this.txtSalario, "Ingrese Descripción");
+        }
+
+        private void txtEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Patrón de expresión regular para permitir solo dígitos
+            string pattern = @"^[0-9]+$";
+            if (!Regex.IsMatch(e.KeyChar.ToString(), pattern) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+                //MessageBox.Show("Solo se permite ingresar valores numéricos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Patrón de expresión regular para permitir solo letras y espacios
+            string pattern = @"^[a-zA-Z\s]+$";
+
+            if (!Regex.IsMatch(e.KeyChar.ToString(), pattern) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten letras", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                //return;
+            }
+        }
+
+        private void txtSalario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string input = (sender as TextBox).Text + e.KeyChar;
+
+            // Patrón de expresión regular que permite números y un solo punto decimal
+            string pattern = @"^[0-9]*(\.[0-9]*)?$";
+
+            if (!Regex.IsMatch(input, pattern) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se permiten números y un punto decimal", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void lklSalir_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtEdad.Clear();  
+            txtSalario.Clear(); 
+            txtDescr.Clear();
+            txtNombre.Focus();  
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Datos agregados correctamnte", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("¿Está seguro que desea cerrar la aplicación",
+               "Info", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.No)
+            {
+                txtNombre.Focus();
+            }
+            else
+            {
+
+                this.Close();
+
+            }
+        }
+
+    }
+}
